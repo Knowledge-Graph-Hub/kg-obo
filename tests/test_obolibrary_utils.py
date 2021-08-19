ba81d41377ab88687f11a626a50555494fdbaf75
Justin Reese <justaddcoffee@gmail.com>
2021-08-19 15:42:50 -0700

    Add test for base_url_if_exists

---
+++ tests/test_obolibrary_utils.py
@@ -0,0 +1,23 @@
+import os
+import tempfile
+from unittest import TestCase, mock
+from unittest.mock import Mock
+
+from kg_obo.obolibrary_utils import base_url_if_exists
+
+
+class TestOboLibraryUtils(TestCase):
+
+    def setUp(self) -> None:
+        pass
+
+    def test_reality(self):
+        self.assertEqual(1, 1)
+
+    @mock.patch('requests.head')
+    @mock.patch('urllib.request.urlopen')
+    def test_download(self, mock_urlopen, mock_head):
+        mock_head.return_value = Mock(status_code=200)
+        base_url_if_exists("test")
+        self.assertTrue(mock_head.called)
+        self.assertTrue(mock_urlopen.called)
