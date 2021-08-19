a3ad98fde7b92d37653d8831b336422db115bff7
Justin Reese <justaddcoffee@gmail.com>
2021-08-19 11:37:02 -0700

    Add to setup.py: test dependencies, correct package name, add Harry as author

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
 from setuptools import setup
 
+test_deps = [
+    'pytest',
+    'pytest-cov',
+    'coveralls',
+    'validate_version_code',
+    'codacy-coverage',
+    'parameterized'
+]
+
+extras = {
+    'test': test_deps,
+}
+
 setup(
-    name='obo2kghub',
+    name='kg-obo',
     version='0.1',
     packages=[''],
-    url='https://github.com/justaddcoffee/obo2kghub',
+    url='https://github.com/Knowledge-Graph-Hub/kg-obo',
     license='',
-    author='Justin Reese',
-    author_email='justinreese@lbl.gov',
+    author='Justin Reese, Harry Caufield',
+    author_email='justinreese@lbl.gov, jhc@lbl.gov',
     description='',
 
     # add package dependencies
