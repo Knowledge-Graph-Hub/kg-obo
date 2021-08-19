6e03a6f0d6406b6aaa1db7c3645581592b2c6f1a
Justin Reese <justaddcoffee@gmail.com>
2021-08-19 16:39:54 -0700

    Add another test

---
+++ tests/test_upload_dir_to_s3.py
@@ -0,0 +1,19 @@
+from unittest import TestCase, mock
+from unittest.mock import Mock
+
+from kg_obo import upload_dir_to_s3
+
+
+class TestUploadDirToS3(TestCase):
+
+    def setUp(self) -> None:
+        pass
+
+    @mock.patch('boto3.client')
+    def test_base_url_if_exists(self, mock_boto):
+        local_dir = "tests/resources/fake_upload_dir"
+        bucket = "my_bucket"
+        bucket_dir = "remote_dir"
+        upload_dir_to_s3(local_dir, bucket, bucket_dir)
+        self.assertTrue(mock_boto.called)
+
