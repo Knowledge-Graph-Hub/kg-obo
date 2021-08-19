262bab9b360af9ef4a61e49b8590599eff60e529
Justin Reese <justaddcoffee@gmail.com>
2021-08-19 16:14:19 -0700

    Add another test

--- tests/test_robot_utils.py
+++ tests/test_robot_utils.py
@@ -1,6 +1,7 @@
+import os.path
 from unittest import TestCase, mock
 
-from kg_obo.robot_utils import convert_owl_to_json
+from kg_obo.robot_utils import convert_owl_to_json, initialize_robot
 
 
 class TestRobotUtils(TestCase):
@@ -10,7 +11,14 @@ class TestRobotUtils(TestCase):
 
     @mock.patch('subprocess.call')
     def test_convert_owl_to_json(self, mock_call):
-        ret = convert_owl_to_json("arg1", "arg2")
+        path = '/some/path'
+        fake_file = '/path/to/uber_ontology.owl'
+        ret = convert_owl_to_json(path, fake_file)
         self.assertTrue(mock_call.called)
-        self.assertEqual(ret, "arg2.json")
+        self.assertEqual(ret, fake_file + '.json')
 
+    def test_initialize_robot(self):
+        path = '/some/path'
+        robot_info = initialize_robot(path)
+        self.assertEqual(robot_info[0], os.path.join(path, "robot"))
+        self.assertEqual(robot_info[1]['ROBOT_JAVA_ARGS'], '-Xmx12g -XX:+UseG1GC')
