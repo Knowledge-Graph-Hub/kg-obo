317029ba35662ba40874450994cb45185b11b0ec
Justin Reese <justaddcoffee@gmail.com>
2021-08-23 17:38:56 -0700

    Move stuff into kg_obo to make it easier to test it

---
+++ kg_obo/transform.py
@@ -0,0 +1,101 @@
+import tempfile
+from kgx.cli import transform  # type: ignore
+from tqdm import tqdm  # type: ignore
+import yaml  # type: ignore
+import requests  # type: ignore
+from datetime import datetime
+import os
+import logging
+import sys
+
+from xml.sax._exceptions import SAXParseException  # type: ignore
+
+from kg_obo.obolibrary_utils import base_url_if_exists
+
+
+def retrieve_obofoundry_yaml(
+        yaml_url: str = 'https://raw.githubusercontent.com/OBOFoundry/OBOFoundry.github.io/master/registry/ontologies.yml',
+        skip_list: list = None) -> list:
+    """ Retrieve YAML containing list of all ontologies in OBOFoundry
+    :param yaml_url: a stable URL containing a YAML file that describes all the OBO ontologies:
+    :param skip_list: which ontologies should we skip
+    :return: parsed yaml describing ontologies to transform
+    """
+    yaml_req = requests.get(yaml_url)
+    yaml_content = (yaml_req.content).decode('utf-8')
+    yaml_parsed = yaml.safe_load(yaml_content)
+    yaml_onto_list = yaml_parsed['ontologies']
+    yaml_onto_list_filtered = [ontology for ontology in yaml_onto_list if
+                               ontology['id'] not in skip_list]
+    return yaml_onto_list_filtered
+
+
+def run_transform(skip_list: list = None) -> None:
+
+    # Set up logging
+    timestring = (datetime.now()).strftime("%Y-%m-%d_%H%M%S")
+    logging.basicConfig(filename=os.path.join("logs", "obo_transform_" + timestring + ".log"),
+                        level=logging.INFO)
+    logger = logging.getLogger()
+
+    yaml_onto_list_filtered = retrieve_obofoundry_yaml(skip_list=skip_list)
+
+    for ontology in tqdm(yaml_onto_list_filtered, "processing ontologies"):
+        ontology_name = ontology['id']
+        print(f"{ontology_name}")
+        logger.info("Loading " + ontology_name)
+
+        url = base_url_if_exists(
+            ontology_name)  # take base ontology if it exists, otherwise just use non-base
+        print(url)
+
+        # download url to tempfile
+        # use kgx to convert OWL to KGX tsv
+        with tempfile.NamedTemporaryFile(prefix=ontology_name) as tfile:
+
+            success = True
+
+            req = requests.get(url, stream=True)
+            file_size = int(req.headers['Content-Length'])
+            chunk_size = 1024
+            with open(tfile.name, 'wb') as outfile:
+                pbar = tqdm(unit="B", total=file_size, unit_scale=True,
+                            unit_divisor=chunk_size)
+                for chunk in req.iter_content(chunk_size=chunk_size):
+                    if chunk:
+                        pbar.update(len(chunk))
+                        outfile.write(chunk)
+            pbar.close()
+
+            tf_output_dir = tempfile.mkdtemp(prefix=ontology_name)
+
+            # Use kgx to transform, but save errors to log
+            try:
+                stderr = sys.stderr
+                stdout = sys.stdout
+                stderr.write = logger.error
+                stdout.write = logger.warning
+                transform(inputs=[tfile.name],
+                          input_format='owl',
+                          output=os.path.join(tf_output_dir, ontology_name),
+                          output_format='tsv',
+                          )
+                sys.stderr = stderr
+                sys.stdout = stdout
+            except FileNotFoundError as e:
+                logger.error(e)
+                success = False
+            except SAXParseException as e:
+                logger.error(e)
+                success = False
+
+            if success:
+                logger.info("Successfully completed transform of " + ontology_name)
+            else:
+                logger.warning("Encountered errors while transforming " + ontology_name)
+
+        # query kghub/[ontology]/current/*hash*
+
+        # kghub/obo2kghub/bfo/2021_08_16|current/nodes|edges.tsv|date-hash
+        os.system(f"ls -lhd {tf_output_dir}/*")
+        # upload to S3
