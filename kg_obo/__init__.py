852e6a4e13aa32fada534f1c4febc4ad6ad56c42
Harry Caufield <j.harry.caufield@gmail.com>
2021-08-27 10:40:42 -0400

    Add upload_index_files

--- kg_obo/__init__.py
+++ kg_obo/__init__.py
@@ -1,7 +1,7 @@
 
 from .obolibrary_utils import base_url_if_exists
 from .transform import retrieve_obofoundry_yaml, kgx_transform, get_owl_iri, track_obo_version, download_ontology, run_transform
-from .upload import upload_dir_to_s3
+from .upload import upload_dir_to_s3, upload_index_files
 
 __all__ = [
     "base_url_if_exists", "retrieve_obofoundry_yaml", "kgx_transform", "get_owl_iri", "track_obo_version", "download_ontology", "run_transform", "upload_dir_to_s3", "update_index_files"  
