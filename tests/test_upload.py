fceabf31fbc16ef1e639ab11b20b82eba464e2df
caufieldjh <j.harry.caufield@gmail.com>
2021-09-03 22:11:18 -0400

    Fix upload test

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -16,6 +16,7 @@ class TestUploadDirToS3(TestCase):
         self.assertTrue(mock_boto.called)
 
     # This is essentially testing a test
+    @mock.patch('boto3.client')
     def test_mock_upload_dir_to_s3(self):
         mock_upload_dir_to_s3(self.local_dir, self.bucket, self.bucket_dir)
         self.assertTrue(mock_boto.called)
