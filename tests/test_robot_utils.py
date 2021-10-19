0bd173a9d127db19d4c7f34e97a5dcc9b4b3cc67
caufieldjh <j.harry.caufield@gmail.com>
2021-10-18 22:29:16 -0400

    Fixes for test_robot_utils

--- tests/test_robot_utils.py
+++ tests/test_robot_utils.py
@@ -2,7 +2,8 @@ import os
 from unittest import TestCase, mock
 from unittest.mock import Mock
 
-from kg_obo.robot_utils import relax_owl, merge_and_convert_owl
+from kg_obo.robot_utils import initialize_robot, relax_owl, merge_and_convert_owl
+from post_setup.post_setup import robot_setup
 
 class TestRobotUtils(TestCase):
 
@@ -12,7 +13,11 @@ class TestRobotUtils(TestCase):
         self.output_owl = 'tests/resources/download_ontology/bfo_processed.owl'
 
     def test_relax_owl(self):
+        robot_setup()
+        initialize_robot(self.robot_path)
         relax_owl(self.robot_path, self.input_owl, self.output_owl)
 
     def test_merge_and_convert_owl(self):
+        robot_setup()
+        initialize_robot(self.robot_path)
         relax_owl(self.robot_path, self.input_owl, self.output_owl)
