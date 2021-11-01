04a688942cb42d700393737ffca494f6bc056431
caufieldjh <j.harry.caufield@gmail.com>
2021-11-01 17:24:53 -0400

    Update stats tests

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -2,8 +2,8 @@ import os
 from unittest import TestCase, mock
 from unittest.mock import Mock
 
-from kg_obo.stats import retrieve_tracking, write_stats, get_file_metadata, \
-                            get_graph_details, get_graph_stats
+from kg_obo.stats import retrieve_tracking, write_stats, get_clean_file_metadata, \
+                            get_graph_details, get_all_stats
                             
 class TestStats(TestCase):
 
@@ -37,20 +37,22 @@ class TestStats(TestCase):
             self.assertTrue(statsfile.read())
 
     @mock.patch('boto3.client')    
-    def test_get_file_metadata(self, mock_boto):
-        mlist = get_file_metadata(self.bucket, self.bucket_dir, 
+    def test_get_clean_file_metadata(self, mock_boto):
+        mlist = get_clean_file_metadata(self.bucket, self.bucket_dir, 
                                     self.versions)
         self.assertTrue(len(mlist)==0) #This mock bucket is empty.
         self.assertTrue(mock_boto.called)
 
-    # Incomplete while the function is incomplete
-    def test_get_graph_details(self):
+    @mock.patch('boto3.client')  
+    def test_get_graph_details(self, mock_boto):
         glist = get_graph_details(self.bucket, self.bucket_dir, 
                                     self.versions)
+        #self.assertTrue(len(glist)==0) #This mock bucket is empty.
+        self.assertTrue(mock_boto.called)
 
     @mock.patch('boto3.client')
     @mock.patch('kg_obo.stats.retrieve_tracking')
-    def test_get_graph_stats(self, mock_boto, mock_retrieve_tracking):
-        get_graph_stats()
+    def test_get_all_stats(self, mock_boto, mock_retrieve_tracking):
+        get_all_stats()
         self.assertTrue(mock_boto.called)
 
