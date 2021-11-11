a779ffab719b3d04026b54cd6c9f1d6b95f40675
caufieldjh <j.harry.caufield@gmail.com>
2021-11-11 12:16:32 -0500

    Add function to test if base-obo URL exists

--- kg_obo/obolibrary_utils.py
+++ kg_obo/obolibrary_utils.py
@@ -3,6 +3,10 @@ import requests  # type: ignore
 
 
 def get_url(oid):
+    """
+    Retrieve what should be a valid URL for a provided OBO ID,
+    though don't check that here.
+    """
     ourl = f"http://purl.obolibrary.org/obo/{oid}/{oid}.owl"
     try:
         ret = requests.head(ourl, allow_redirects=True)
@@ -14,10 +18,34 @@ def get_url(oid):
                 i = i + 1
                 if i > 3:
                     break
-                l = line.decode('utf-8')
-                if "ListBucketResult" in l:
+                if "ListBucketResult" in line:
                     ourl = f"http://purl.obolibrary.org/obo/{oid}.owl"
 
     except Exception:
         ourl = f"http://purl.obolibrary.org/obo/{oid}.owl"
+
     return ourl
+
+def base_url_exists(oid):
+    """
+    Returns True if a base version of the provided OBO ID
+    exists, as this means we likely used it in the past.
+    """
+    base_ourl = f"http://purl.obolibrary.org/obo/{oid}/{oid}-base.owl"
+    base_exists = True
+    try:
+        ret = requests.head(base_ourl, allow_redirects=True)
+        if ret.status_code != 200:
+            base_exists = False
+        else:
+            i = 0
+            for line in urllib.request.urlopen(base_ourl):
+                i = i + 1
+                if i > 3:
+                    break
+                if "ListBucketResult" in line:
+                    base_exists = False
+    except Exception as e:
+        base_exists = False
+
+    return base_exists
--- tests/test_obolibrary_utils.py
+++ tests/test_obolibrary_utils.py
@@ -1,12 +1,13 @@
 from unittest import TestCase, mock
 from unittest.mock import Mock
 
-from kg_obo.obolibrary_utils import get_url
+from kg_obo.obolibrary_utils import get_url, base_url_exists
 
 
 class TestOboLibraryUtils(TestCase):
 
     def setUp(self) -> None:
+        self.url_values = "1\n2\n3\n"
         pass
 
     @mock.patch('requests.head')
@@ -14,9 +15,27 @@ class TestOboLibraryUtils(TestCase):
     def test_get_url(self, mock_urlopen, mock_head):
         for status in 200, 404:
             mock_head.return_value = Mock(status_code=status)
-            mock_urlopen.return_value = [1, 2, 3]
+            mock_urlopen.return_value = self.url_values
             ret_url = get_url("test")
             self.assertTrue(mock_head.called)
             self.assertTrue(mock_urlopen.called)
-            self.assertEqual(ret_url, "http://purl.obolibrary.org/obo/test.owl")
+            if status == 200:
+                self.assertEqual(ret_url, "http://purl.obolibrary.org/obo/test/test.owl")
+            else:
+                self.assertEqual(ret_url, "http://purl.obolibrary.org/obo/test.owl")
+            
+
+    @mock.patch('requests.head')
+    @mock.patch('urllib.request.urlopen')
+    def test_base_url_exists(self, mock_urlopen, mock_head):
+        for status in 200, 404:
+            mock_head.return_value = Mock(status_code=status)
+            mock_urlopen.return_value = self.url_values
+            base_exists = base_url_exists("test")
+            self.assertTrue(mock_head.called)
+            self.assertTrue(mock_urlopen.called)
+            if status == 200:
+                self.assertTrue(base_exists)
+            else:
+                self.assertFalse(base_exists)
 
